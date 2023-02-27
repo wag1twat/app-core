0f690222c095067cfd6db83780ff1a1e0193c9ad
wag1twat <up.pavlogradsky@gmail.com>
2023-02-27 03:25:28 +0300

    fix/array-sort-perfomance-test

--- tests/perfomance/array-sort.spec.ts
+++ tests/perfomance/array-sort.spec.ts
@@ -44,19 +44,20 @@ describe('array-sort-perfomance', () => {
     })
     test('$Array sort 1000000', () => {
         let t0 = performance.now()
-        let order: Types.Array.Sort.Order = 'DESC'
+        let order!: Types.Array.Sort.Order
         const array = $Array(users1000000).sort({
             field: 'address.geo.lat',
-            order,
+            order: 'ASC',
             orders: ['ASC', 'DESC', 'default'],
             onUpdate(state) {
                 order = state._order
             },
         })
         let t1 = performance.now()
-        result[`${order} $Array sort`] = `${Math.ceil(t1 - t0)} ms`
+        result[`${order} 0 $Array sort`] = `${Math.ceil(t1 - t0)} ms`
 
         t0 = performance.now()
+        array.update()
         t1 = performance.now()
         result[`${order} 1 $Array sort`] = `${Math.ceil(t1 - t0)} ms`
 
